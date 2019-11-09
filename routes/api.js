16e9c686eb431125d5f5390fce1095d37787b723
Michael Lucas <lucasmichael221@gmail.com>
2019-11-09 12:05:31 -0500

    Added environment variable setting and finished api token route

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,11 @@ var request = require('request');
 router.get('/token', (req, res, next) => {
   request
     .post('https://api.petfinder.com/v2/oauth2/token')
-    .json()
+    .json({
+      "grant_type": "client_credentials",
+      "client_id": process.env.CLIENT_ID,
+      "client_secret": process.env.CLIENT_SECRET
+    })
     .on('data', data => {
       res.status(200).send(data);
     })
