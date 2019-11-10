ef983f7bcfc668eec3facf9c24ef65320946ee0f
Michael Lucas <lucasmichael221@gmail.com>
2019-11-10 15:16:51 -0500

    Fixed errors and added post for keyword search

--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,47 @@ const router = express.Router();
 const request = require('request');
 const util = require('../util');
 
+/* POST route for keyword search */
+router.post('/', (req, res, next) => {
+  let urlParams = '';
+  for (let prop in req.body) {
+    if (req.body.hasOwnProperty(prop)) {
+      urlParams += prop + '=' + req.body[prop] + '&';
+    }
+  }
+  
+  util.getToken()
+    .then(token => {
+      const options = {
+        method: 'GET',
+        url: 'https://api.petfinder.com/v2/animals?' + urlParams.slice(0, urlParams.length - 1),
+        headers: {
+          'Authorization': 'Bearer ' + token
+        },
+        json: true
+      };
+
+      request(options, (err, _, body) => {
+        if (err) {
+          console.log(err);
+        }
+
+        const msg = body.animals.map(x => {
+          console.log(x)
+          return {
+            name: x.name,
+            photo: x.photos[0] ? x.photos[0].full : null,
+            description: x.description,
+            breed: x.breeds.primary
+          };
+        });
+
+        res.status(200).send(msg);
+      })
+    })
+    .catch(err => {
+      return err;
+    })
+});
 
 module.exports = router;
--- util.js
+++ util.js
@@ -1,21 +1,29 @@
-const request = require('request');
+const rp = require('request-promise-native');
 
-const getToken = () => {
-  request
-    .post('https://api.petfinder.com/v2/oauth2/token')
-    .json({
+async function getToken() {
+  const options = {
+    method: 'POST',
+    url: 'https://api.petfinder.com/v2/oauth2/token',
+    json: {
       "grant_type": "client_credentials",
       "client_id": process.env.CLIENT_ID,
       "client_secret": process.env.CLIENT_SECRET
+    }
+  }
+
+  let token;
+
+  await rp(options)
+    .then(body => {
+      token = body.access_token;
     })
-    .on('data', data => {
-      res.status(200).send(data);
-    })
-    .on('error', err => {
+    .catch(err => {
       console.log(err);
-    });
+    })
+
+  return token;
 }
 
-export default {
+module.exports = {
   getToken
 };
