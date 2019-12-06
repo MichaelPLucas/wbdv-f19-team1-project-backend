0c2e21ff32dd0897b5734cda025fc796f694de12
Michael Lucas <lucasmichael221@gmail.com>
2019-12-05 19:54:14 -0500

    Sanitized routes to hide passwords and added a login route that confirms the password

--- routes/user.js
+++ routes/user.js
@@ -17,21 +17,30 @@ module.exports = function (app) {
 		console.log('findAllUsers')
 		userDao
 			.findAllUsers()
-			.then(users => res.send(users))
+			.then(users => {
+        users.map(x => x.password = undefined);
+        res.send(users);
+      })
 	}
 
 	function findUserById(req, res) {
 		var userId = req.params['sid'];
 		userDao
 			.findUserById(userId)
-			.then(user => res.json(user))
+			.then(user => {
+        user.password = undefined;
+        res.json(user);
+      })
 	}
 
 	function findUserByUsername(req, res) {
 		var username = req.params['username'];
 		userDao
 			.findUserByUsername(username)
-			.then(user => res.json(user))
+			.then(user => {
+        user.password = undefined;
+        res.json(user);
+      })
 	}
 
 	function updateUser(req, res) {
@@ -47,12 +56,30 @@ module.exports = function (app) {
 		userDao
 			.deleteUser(userId)
 			.then(status => res.json(status))
-	}
+  }
+  
+  function login(req, res) {
+    let username = req.params.username;
+    let password = req.params.password;
+    userDao.findUserByUsername(username)
+      .then(user => {
+        if (!user)
+          return
+
+        if (user.password === password) {
+          user.password = undefined;
+          res.json(user);
+        } else {
+          res.status(400).json({ error: 'permission denied' });
+        }
+      });
+  }
 
 	app.post('/users', createUser);
 	app.get('/users', findAllUsers);
 	app.get('/users/:sid', findUserById);
 	app.get('/users/username/:username', findUserByUsername);
 	app.put('/users/:sid', updateUser);
-	app.delete('/users/:sid', deleteUser);
+  app.delete('/users/:sid', deleteUser);
+  app.get('/users/login/:username/:password', login);
 }
